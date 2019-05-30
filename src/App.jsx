248022e483f89db1587c348ec8c9f47f23503135
Michał Szumnarski <michal.szumnarski@gmail.com>
2019-05-30 12:58:48 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+﻿import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
+import Blank from "./components/Blank";
+import Grid from "./components/Grid";
+import List from "./components/List";
+import Master_Detail from "./components/Master_Detail";
+import Blank2 from "./components/Blank2";
+import Grid2 from "./components/Grid2";
+import List2 from "./components/List2";
+import Master_Detail2 from "./components/Master_Detail2";
+//TODO Web Template Studio: Add routes for your new pages here.
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <Switch>
+          < Redirect exact path = "/" to = "/Blank" />
+          < Route path = "/Blank" component = { Blank } />
+          < Route path = "/Grid" component = { Grid } />
+          < Route path = "/List" component = { List } />
+          < Route path = "/Master_Detail" component = { Master_Detail } />
+          < Route path = "/Blank2" component = { Blank2 } />
+          < Route path = "/Grid2" component = { Grid2 } />
+          < Route path = "/List2" component = { List2 } />
+          < Route path = "/Master_Detail2" component = { Master_Detail2 } />
+        </Switch>
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,45 @@
+﻿import React from "react";
+import styles from "./navbar.module.css";
+
+//TODO Web Template Studio: Add a new link in the NavBar for your page here.
+// A skip link is included as an accesibility best practice. For more information visit https://www.w3.org/WAI/WCAG21/Techniques/general/G1.
+export default function NavBar() {
+  return (
+    <React.Fragment>
+      <div className={styles.skipLink}>
+        <a href="#mainContent">Skip to Main Content</a>
+      </div>
+      <nav className="navbar navbar-expand-sm navbar-light border-bottom justify-content-between">
+        <a className="navbar-brand" href="/">
+          databaseTalker
+        </a>
+        <div className="navbar-nav">
+          <a className="nav-item nav-link active" href="Blank">
+            Blank
+          </a>
+          <a className="nav-item nav-link active" href="Grid">
+            Grid
+          </a>
+          <a className="nav-item nav-link active" href="List">
+            List
+          </a>
+          <a className="nav-item nav-link active" href="Master_Detail">
+            Master_Detail
+          </a>
+          <a className="nav-item nav-link active" href="Blank2">
+            Blank2
+          </a>
+          <a className="nav-item nav-link active" href="Grid2">
+            Grid2
+          </a>
+          <a className="nav-item nav-link active" href="List2">
+            List2
+          </a>
+          <a className="nav-item nav-link active" href="Master_Detail2">
+            Master_Detail2
+          </a>
+        </div>
+      </nav>
+    </React.Fragment>
+  );
+}
